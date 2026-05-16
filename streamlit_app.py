4a43cc1bbc07dc4ffed3766ec838104d935efd28
moonbuggy42 <mihin0011@gmail.com>
2026-05-16 13:16:46 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ import numpy as np
 import plotly.express as px
 st.title('Modelling Exponential Growth and Decay')
 
-st.write('Hello world!')
+st.subheader('Richter Scale: Q2')
+df1 = pd.read_excel(r"C:/Users/Mihin/Documents/Richter Scale .xlsx", index_col=0)
